1103bd02f4698c3a7a032b8b48e6094276538d75
imusky <116124323+imusky@users.noreply.github.com>
2022-11-04 15:48:44 -0300

    criei o modulo001 e um exercicio
    Criei a pasta do modulo 001 e um exercício que tem a função de mostrar algo na tela.

---
+++ modulo001-tipos-de-variaveis/hello-world/01hello-world.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Função principal do programa
+void main(){
+
+    //Imprime na tela
+    printf("Hello World!");
+
+    //Pausa o programa após executar
+    system("pause");
+
+    //Retorna o valor 0 se utilizar int
+    //return 0;
+}
