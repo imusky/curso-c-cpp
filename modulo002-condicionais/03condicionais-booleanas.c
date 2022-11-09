c68d0392562e54b733c5e1a9fcb230ec93b331b3
imusky <116124323+imusky@users.noreply.github.com>
2022-11-09 18:28:18 -0300

    criei o exercicio 03condicionais-booleanas
    Criei o exercício 03condicionais-booleanas

---
+++ modulo002-condicionais/03condicionais-booleanas.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void main(){
+
+    bool a = true, b = false;
+
+    //se A for verdadeiro
+    if(a){
+        printf("A eh verdadeiro\n");
+    }
+
+    //se B for falso
+    if(b){
+        printf("B eh verdadeiro\n");
+    }else{
+        printf("B eh falso\n");
+    }
+
+    //comparando uma falsidade
+    if (!b){
+        printf("B eh falso\n");
+    }
+
+    system("pause");
+}
