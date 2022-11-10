e0b9a8cf336fadd3853475a13a0781251eaf5126
imusky <116124323+imusky@users.noreply.github.com>
2022-11-10 17:59:21 -0300

    Create 04condicionais-com-char.c
    Criei o exercício 04 condicionais-com-char, nele aprendi a ver códigos ascii e como utilizar as condicionais com char.

---
+++ modulo002-condicionais/04condicionais-com-char.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+
+    char letra = 'x';
+
+    //condicional simples
+    if(letra == 'x'){
+        printf("A letra eh x.\n");
+    }
+
+    printf("Codigo da letra = %d", letra);
+    if(letra == 120){
+        printf("A letra eh x. \n");
+    }
+    //também posso ver codigo da letra em https://ascii.cl/
+
+    system("pause");
+}
