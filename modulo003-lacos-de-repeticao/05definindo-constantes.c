989464c8a1dd2118a87a74d624c1414c325537a8
imusky <116124323+imusky@users.noreply.github.com>
2022-11-15 15:18:54 -0300

    criei o ex05definindo-constantes
    Criei o ex05definindo-constantes e nele aprendi a definir constantes.

---
+++ modulo003-lacos-de-repeticao/05definindo-constantes.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define TAM 10
+
+void main(){
+
+    printf("%d", TAM);
+
+    int i;
+
+    //contagem ate 10
+    for(i = 1; i <= TAM; i++){
+        printf("%d\n", i);
+    }
+
+    system("pause");
+}
