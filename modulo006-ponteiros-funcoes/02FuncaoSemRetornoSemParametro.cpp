0efcaffc3b4af183a9489a7124f5200a85894a89
imusky <116124323+imusky@users.noreply.github.com>
2022-11-22 17:49:38 -0300

    Create 02FuncaoSemRetornoSemParametro.cpp
    Aprendi a utilizar funções.

---
+++ modulo006-ponteiros-funcoes/02FuncaoSemRetornoSemParametro.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//funcao que desenha borda
+void desenhaBorda(){
+    printf("|----|\n");
+}
+
+int main(){
+
+    //chamando a funcao
+    desenhaBorda();
+
+    printf("oi\n");
+    
+    //chamando a funcao
+    desenhaBorda();
+
+    return 0;
+}
