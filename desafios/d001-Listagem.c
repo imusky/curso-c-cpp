880fd73151bb93f5b970b0304c90b3650cf67447
imusky <116124323+imusky@users.noreply.github.com>
2022-11-25 13:47:52 -0300

    Criei o d001
    Criei o desafio 001, aonde tive que imprimir valores na tela.

---
+++ desafios/d001-Listagem.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main(){
+    printf("<<< EX001 - LISTAGEM >>>\n");
+    printf("\n");
+    printf("Listagem de Alunos\n");
+    printf("Nome \t\tNota\n");
+    printf("---------------------\n");
+    printf("Ana Beatriz\t8.5\n");
+    printf("Bianca Martins\t9.0\n");
+    printf("Claudio Sa\t5.5\n");
+    printf("Giovana Silva\t7.5\n");
+}
