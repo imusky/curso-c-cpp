53e98bd8b83d1b1183171f5c8b3825eb55dba8ab
imusky <116124323+imusky@users.noreply.github.com>
2022-11-25 14:05:03 -0300

    Criei o d002 especiais
    Criei o desafio 002 Especiais, aonde tive que mostrar caracteres especiais e suas funções.

---
+++ desafios/d002-Especiais.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main(){
+
+    printf("<<< EX002 - ESPECIAIS >>>\n\n");
+    printf("=-=-=--=-=-=-=-=-=-==-=-=--=-=-=\n");
+    printf("\\a\t=\t Beep\n");
+    printf("\\n\t=\t Nova linha\n");
+    printf("\\t\t=\t Tabulacao\n");
+    printf("\\\\\t=\t Barra\n");
+    printf("%%%%\t=\t Porcentagem\n");
+    printf("\\?\t=\t Interrogacao\n");
+    printf("=-=-=--=-=-=-=-=-=-==-=-=--=-=-=");
+}
